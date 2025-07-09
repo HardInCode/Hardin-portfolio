efa7086d901fc3b6fa99a886ad32dd6446a5c1a7
IrpanXD <hardinirfan31@gmail.com>
2025-07-09 08:08:41 +0700

    some update

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -132,7 +132,7 @@ const Experience = () => {
                         <p className="my-5 text-white-50">
                           🗓️&nbsp;{card.date}
                         </p>
-                        <p className="text-[#839CB5] italic">
+                        <p className="text-[#8a8a8a] italic">
                           Responsibilities
                         </p>
                         <ul className="list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50">
